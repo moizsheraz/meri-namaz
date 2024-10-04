8e42bfb27c8a6452b344c1eb1e6616f0af172518
moizsheraz <user146712@gmail.com>
2024-10-04 06:28:24 +0500

    frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
    "use client"
-import Image from "next/image";
 import { useState, useEffect } from 'react'
-import { Bell, Book, Calendar, ChevronRight, Compass, Heart, MessageCircle, Moon, Sun } from 'lucide-react'
+import { Bell, Book, Compass, Heart, MessageCircle, Moon, Sun } from 'lucide-react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
